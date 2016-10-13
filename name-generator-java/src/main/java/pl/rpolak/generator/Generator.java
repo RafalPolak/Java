b8a779460d84ee9e6c70fed775dc5f664ab08ee9
rafal.polak <thepatriot91@hotmail.com>
2016-10-13 17:31:29 +0200

    Added 12 tests for program Cleaning code

--- name-generator-java/src/main/java/pl/rpolak/generator/Generator.java
+++ name-generator-java/src/main/java/pl/rpolak/generator/Generator.java
@@ -14,7 +14,9 @@ public class Generator {
 
         /*
         First choose what we want to do generate name/password or both
-        Next if name then select man/women if password select lenght
+        Next: 
+            - if name then select man/women 
+            - if password select lenght
          */
         int menuSelect = 1;
         int genderSelect = 0;
@@ -37,13 +39,13 @@ public class Generator {
                     + "3 --> generate name with surname and password\n"
                     + "0 --> quit");
 
-            try{
+            try {
                 menuSelect = scanner.nextInt();
-            } catch (InputMismatchException inputMismatchException){
-                throw new InputMismatchException("Wrong parameter!!! "+inputMismatchException);
+            } catch (InputMismatchException inputMismatchException) {
+                throw new InputMismatchException("Wrong parameter!!! " + inputMismatchException);
             }
 
-            System.out.println("Your selection: " + menuSelect+"\n");
+            System.out.println("Your selection: " + menuSelect + "\n");
 
             //Create console questions for choose gender/password length or both
             if (menuSelect == 1) {
@@ -52,18 +54,16 @@ public class Generator {
                         + "1 --> male\n"
                         + "2 --> female\n");
 
-                //toDo security for value != (1 and 2)
-                
-                try{
+                try {
                     genderSelect = scanner.nextInt();
                 } catch (InputMismatchException inputMismatchException) {
-                    throw new InputMismatchException("Wrong parameter!!! "+inputMismatchException);
+                    throw new InputMismatchException("Wrong parameter!!! " + inputMismatchException);
                 }
-                
-                if(genderSelect != 1 || genderSelect != 2){
+
+                if (genderSelect != 1 || genderSelect != 2) {
                     throw new InputMismatchException("Wrong parameter !!!\nThere should be 1 or 2 ");
                 }
-                
+
                 //Use generator
                 nameData = generator.generateName(genderSelect).toString();
             }
@@ -72,12 +72,12 @@ public class Generator {
                 System.out.println("You choose to generate password\n"
                         + "How long should password be ?");
 
-                try{
+                try {
                     passwordLenght = scanner.nextInt();
-                } catch (InputMismatchException inputMismatchException){
-                    throw new InputMismatchException("Wrong parameter!!! "+inputMismatchException);
+                } catch (InputMismatchException inputMismatchException) {
+                    throw new InputMismatchException("Wrong parameter!!! " + inputMismatchException);
                 }
-                
+
                 //Use generator
                 password = generator.generateCredential(passwordLenght).toString();
 
@@ -94,15 +94,14 @@ public class Generator {
 
                 System.out.println("How long should password be?");
 
-                //toDo security for incorrect lenght or provided letter
                 passwordLenght = scanner.nextInt();
 
                 nameData = generator.generateName(genderSelect).toString();
                 password = generator.generateCredential(passwordLenght).toString();
 
             }
-            
-            if (menuSelect == 0){
+
+            if (menuSelect == 0) {
                 System.exit(menuSelect);
             }
 
