201f898d571ff0f34944eee21e1d14830d7915e8
rafal.polak <thepatriot91@hotmail.com>
2016-10-21 20:57:48 +0200

    Create formulas for converts celsius to other temperatures Create util class with all necessary constants for equations

---
+++ TemperatureConverter/src/main/java/pl/rpolak/temperatureconverter/celsius/bo/CelsiusConverterBO.java
@@ -0,0 +1,44 @@
+package pl.rpolak.temperatureconverter.celsius.bo;
+
+import pl.rpolak.temperatureconverter.util.ConstantsForEquations;
+
+/**
+ *
+ * @author Rafal.Polak
+ */
+public class CelsiusConverterBO implements ICelsiusConverterBO {
+
+    /*
+    0 degrees Celsius is equal to 32 degrees Fahrenheit
+        celsius * 1.8 + 32
+     */
+    @Override
+    public double toFahrenheit(double celsius) {
+        return (celsius * ConstantsForEquations.CONSTANT_2) + ConstantsForEquations.CONSTANT_1;
+    }
+
+    /*
+    The temperature in Kelvin is equal to the temperature in degrees Celsius plus 273.15
+     */
+    @Override
+    public double toKelvin(double celsius) {
+        return celsius + ConstantsForEquations.CONSTANT_3;
+    }
+
+    @Override
+    /*
+    The temperature in Rankine is equal to the temperature in degrees Celsius * 1.8 + 32 + 459.67
+     */
+    public double toRankine(double celsius) {
+        return (celsius * ConstantsForEquations.CONSTANT_2) + ConstantsForEquations.CONSTANT_1 + ConstantsForEquations.CONSTANT_5;
+    }
+
+    /*
+    The temperature in Reaumur is equal to the temperature in degrees Celsiusu * 0.8
+     */
+    @Override
+    public double toReaumur(double celsius) {
+        return celsius * ConstantsForEquations.CONSTANT_4;
+    }
+
+}
